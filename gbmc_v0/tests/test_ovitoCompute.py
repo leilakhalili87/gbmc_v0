5bd1e207fe899329551b7b778445161ef176d4f1
lkhalil <lkhalil@ncsu.edu>
2020-05-15 14:50:27 -0400

    add

--- gbmc_v0/tests/test_ovitoCompute.py
+++ gbmc_v0/tests/test_ovitoCompute.py
@@ -2,7 +2,7 @@ from ..util_funcs import compute_ovito_data
 
 
 def test_compute_ovito_data():
-    filename0 = "./data/dump_1"
+    filename0 = "/data/dump_1"
     actual = 3588
     expected = compute_ovito_data(filename0)
     assert actual == expected
