54eb214580584a5c6afcb1e56c181fe636a3c955
lkhalil <lkhalil@ncsu.edu>
2020-05-15 14:21:33 -0400

    fix test

--- gbmc_v0/tests/test_ovitoCompute.py
+++ gbmc_v0/tests/test_ovitoCompute.py
@@ -1,10 +1,8 @@
-import numpy as np
-import ovito.io as oio
-import ovito.modifiers as ovm
-
-from gbmc_v0 import *
+from ..util_funcs import compute_ovito_data
 
 filename0 = "./data/dump_1"
+
+
 def test_compute_ovito_data(filename0):
     actual = 3588
     expected = compute_ovito_data(filename0)
