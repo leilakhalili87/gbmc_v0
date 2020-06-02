e1a3676cf3499c1b95aae0459186d1490ce919ce
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-02 19:47:02 +0000

    Update docs after building Travis build 67 of leilakhalili87/gbmc_v0
    The docs were built from the branch 'master' against the commit
    8823360ff06a70cecffb368e1b5aa8de0c44efcf.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/leilakhalili87/gbmc_v0/jobs/693993103.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.post18+g47ca2d4',
+    VERSION: '1.0.1.post17+g8823360',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
