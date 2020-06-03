79190b0decd67449e419c63a06785fcc4df622d7
lkhalil <lkhalil@ncsu.edu>
2020-06-03 10:07:38 -0400

    clean up the circum_O_R function

--- gbmc_v0/vv_props.py
+++ gbmc_v0/vv_props.py
@@ -3,9 +3,15 @@ from scipy.spatial.distance import pdist
 from pyhull import qdelaunay
 
 
-def Circum_O_R(vertex_pos, tol):
+def Circum_O_R(vertex_pos, tol, method="method_1"):
     """
     Function finds the center and the radius of the circumsphere of the every tetrahedron.
+    This function can use two different methods which can be defind as "method_1" or "method_2.
+    The default method is "method_1".
+    Reference for method_1:
+    Fiedler, Miroslav. Matrices and graphs in geometry. No. 139. Cambridge University Press, 2011.
+    Reference for method_2:
+    https://mathworld.wolfram.com/Circumsphere.html
 
     Parameters
     -----------------
@@ -21,65 +27,52 @@ def Circum_O_R(vertex_pos, tol):
     circum_rad :
         The radius of the circum-sphere
     """
-    # this is the second method just to check
-    # ones_col = np.ones((4,1))
-    # a = np.linalg.det(np.append(vertex_pos, ones_col,1))
-    # A = vertex_pos[0,:]
-    # B = vertex_pos[1,:]
-    # C = vertex_pos[2,:]
-    # D = vertex_pos[3,:]
-    # sq_p1 = np.dot(A,A)
-    # sq_p2 = np.dot(B,B)
-    # sq_p3 = np.dot(C,C)
-    # sq_p4 = np.dot(D,D)
+    if method == "method_1":
+        dis_ij = pdist(vertex_pos, 'euclidean')
+        sq_12, sq_13, sq_14, sq_23, sq_24, sq_34 = np.power(dis_ij, 2)
 
-    # dx = np.array([[sq_p1, A[1], A[2], 1], [sq_p2, B[1], B[2], 1],
-    #  [sq_p3, C[1], C[2], 1], [sq_p4, D[1], D[2], 1]])
-    # Dx = np.linalg.det(dx)
+        MatrixC = np.array([[0, 1, 1, 1, 1], [1, 0, sq_12, sq_13, sq_14], [1, sq_12, 0, sq_23, sq_24],
+                           [1, sq_13, sq_23, 0, sq_34], [1, sq_14, sq_24, sq_34, 0]])
 
-    # dy = np.array([[sq_p1, A[0], A[2], 1], [sq_p2, B[0], B[2], 1],
-    #  [sq_p3, C[0], C[2], 1], [sq_p4, D[0], D[2], 1]])
-    # Dy = -np.linalg.det(dy)
+        det_MC = (np.linalg.det(MatrixC))
 
-    # dz = np.array([[sq_p1, A[0], A[1], 1], [sq_p2, B[0], B[1], 1],
-    #  [sq_p3, C[0], C[1], 1], [sq_p4, D[0], D[1], 1]])
-    # Dz = np.linalg.det(dz)
+        if (det_MC < tol):
+            return [0, 0, 0], 0
+        else:
+            M = -2*np.linalg.inv(MatrixC)
+            circum_center = (M[0, 1]*vertex_pos[0, :] + M[0, 2]*vertex_pos[1, :] + M[0, 3]*vertex_pos[2, :] +
+                             M[0, 4] * vertex_pos[3, :]) / (M[0, 1] + M[0, 2] + M[0, 3] + M[0, 4])
+            circum_rad = np.sqrt(M[0, 0])/2
+    if method == "method_2":
+        ones_col = np.ones((4, 1))
+        a = np.linalg.det(np.append(vertex_pos, ones_col, 1))
+        A = vertex_pos[0, :]
+        B = vertex_pos[1, :]
+        C = vertex_pos[2, :]
+        D = vertex_pos[3, :]
+        sq_p1 = np.dot(A, A)
+        sq_p2 = np.dot(B, B)
+        sq_p3 = np.dot(C, C)
+        sq_p4 = np.dot(D, D)
+        c_mat = np.array([[sq_p1, A[0], A[1], A[2]], [sq_p2, B[0], B[1], B[2]],
+                         [sq_p3, C[0], C[1], C[2]], [sq_p4, D[0], D[1], D[2]]])
+        c = np.linalg.det(c_mat)
+        dx = np.array([[sq_p1, A[1], A[2], 1], [sq_p2, B[1], B[2], 1],
+                      [sq_p3, C[1], C[2], 1], [sq_p4, D[1], D[2], 1]])
+        Dx = np.linalg.det(dx)
 
-    # center = [Dx/(2*a), Dy/(2*a), Dz/(2*a)]
-    # return center
+        dy = np.array([[sq_p1, A[0], A[2], 1], [sq_p2, B[0], B[2], 1],
+                      [sq_p3, C[0], C[2], 1], [sq_p4, D[0], D[2], 1]])
+        Dy = -np.linalg.det(dy)
 
-    # """
-    # This function finds thecenter and the radius of the circumsphere of the every tetrahedron.
-    # We used "Miroslav Fiedler's method" which is based on  "Cayley-Menger matrix".
-    # Ref: https://www.cambridge.org/core/books/matrices-and-graphs-in-geometry/B225AA8C5EA96EAD4D0280173930FE03
-    # ------------------------------------
-    # Parameter:
-    #     vertex_pos: The position of a atoms on vertex.  The dimension of input is (4,3)
-    # -------------------------------------
-    # Return:
-    #     circum_center: The center of the circumsphere
-    #     circum_rad: The radius of the circumsphere
-    # Test case based on this website: ADD IT TO THE TEST CASE???
-    # https://mathematica.stackexchange.com/questions/33846/insphere-for-irregular-tetrahedron/89090#89090
-    # a = np.array([[0,0,0],[1,0,0],[2,1,0],[1,1.5,1]])
-    # answer:array([ 0.5  ,  1.5  , -0.625]), 1.7001838135919305
-    # """
-    dis_ij = pdist(vertex_pos, 'euclidean')
-    sq_12, sq_13, sq_14, sq_23, sq_24, sq_34 = np.power(dis_ij, 2)
+        dz = np.array([[sq_p1, A[0], A[1], 1], [sq_p2, B[0], B[1], 1],
+                      [sq_p3, C[0], C[1], 1], [sq_p4, D[0], D[1], 1]])
+        Dz = np.linalg.det(dz)
 
-    MatrixC = np.array([[0, 1, 1, 1, 1], [1, 0, sq_12, sq_13, sq_14], [1, sq_12, 0, sq_23, sq_24],
-                       [1, sq_13, sq_23, 0, sq_34], [1, sq_14, sq_24, sq_34, 0]])
+        circum_center = [Dx / (2 * a), Dy / (2 * a), Dz / (2 * a)]
+        circum_rad = np.sqrt(Dx * Dx + Dy * Dy + Dz * Dz - 4 * a * c) / 2 / np.abs(a)
 
-    det_MC = (np.linalg.det(MatrixC))
-
-    if (det_MC < tol):
-        return [0, 0, 0], 0
-    else:
-        M = -2*np.linalg.inv(MatrixC)
-        circum_center = (M[0, 1]*vertex_pos[0, :] + M[0, 2]*vertex_pos[1, :] + M[0, 3]*vertex_pos[2, :] +
-                         M[0, 4] * vertex_pos[3, :]) / (M[0, 1] + M[0, 2] + M[0, 3] + M[0, 4])
-        circum_rad = np.sqrt(M[0, 0])/2
-        return circum_center, circum_rad
+    return circum_center, circum_rad
 
 
 def triang_inds(pts_w_imgs, gb1_inds):
