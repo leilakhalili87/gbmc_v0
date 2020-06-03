1cb2e287a79fae998120cf49ef03c6bf9784cb7d
lkhalil <lkhalil@ncsu.edu>
2020-06-03 10:52:56 -0400

    adding tests, test_circum is added

--- gbmc_v0/tests/test_ovitoCompute.py
+++
@@ -1,8 +0,0 @@
-from ..util_funcs import compute_ovito_data
-
-
-def test_compute_ovito_data():
-    filename0 = "gbmc_v0/tests/data/dump_1"
-    actual = 5447
-    expected = data.particles.count
-    assert actual == expected
