07c8049e90f7a81b40c9356259b7d0a044038907
lkhalil <lkhalil@ncsu.edu>
2020-06-04 09:05:43 -0400

    fix an error

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
             # 'path/to/data_file',
-            '../lammps_dump/*'.
+            '../lammps_dump/*',
             'data/dump_1'
         ]
     },
