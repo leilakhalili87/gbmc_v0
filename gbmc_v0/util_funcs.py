6093068eef6301cb0a23a8fc862443f4440f7884
lkhalil <lkhalil@ncsu.edu>
2020-06-12 14:38:27 -0400

    2 new funcs are added. needed for writing lammps script

--- gbmc_v0/util_funcs.py
+++ gbmc_v0/util_funcs.py
@@ -1,7 +1,7 @@
 import numpy as np
 import ovito.io as oio
 import ovito.modifiers as ovm
-
+from itertools import islice 
 
 def compute_ovito_data(filename0):
     """
@@ -43,6 +43,38 @@ def identify_pbc(data):
     pbc = np.asarray(pbc) + 0
     non_pbc = np.where(pbc == 0)[0][0]
     return non_pbc
+
+def box_size_reader(dump_name):
+    with open(dump_name) as lines:
+        box_bound = np.genfromtxt(islice(lines, 5, 8))
+    return box_bound
+
+
+def define_bounds(box_bound):
+    # boundaries of fix rigid
+    siz_box = np.shape(box_bound)[1]
+    if siz_box == 2:
+        box_type = "block"
+        xy = 0
+        xz = 0
+        yz = 0
+        tilt = []
+    else:
+        box_type = "prism"
+        xy = box_bound[0, 2]
+        xz = box_bound[1, 2]
+        yz = box_bound[2, 2]
+        tilt = np.array([xy, xz, yz])
+
+    xlo = box_bound[0, 0] - np.min(np.array([0, xy, xz, xy + xz]))
+    xhi = box_bound[0, 1] - np.max(np.array([0, xy, xz, xy + xz]))
+    ylo = box_bound[1, 0] - np.min(np.array([0, yz]))
+    yhi = box_bound[1, 1] - np.max(np.array([0, yz]))
+    zlo = box_bound[2, 0]
+    zhi = box_bound[2, 1]
+    untilted = np.array([[xlo, xhi], [ylo, yhi], [zlo, zhi]])
+    
+    return untilted, tilt, box_type
 
 
 def RemProb(data, CohEng, GbIndex):
