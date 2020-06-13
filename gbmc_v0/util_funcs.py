4167c9582a5021ac37aa0b733c8c95095731bf50
lkhalil <lkhalil@ncsu.edu>
2020-06-13 15:01:48 -0400

    order some func

--- gbmc_v0/util_funcs.py
+++ gbmc_v0/util_funcs.py
@@ -1,7 +1,9 @@
 import numpy as np
 import ovito.io as oio
 import ovito.modifiers as ovm
-from itertools import islice 
+from itertools import islice
+import pad_dump_file as pdf
+
 
 def compute_ovito_data(filename0):
     """
@@ -44,6 +46,7 @@ def identify_pbc(data):
     non_pbc = np.where(pbc == 0)[0][0]
     return non_pbc
 
+
 def box_size_reader(dump_name):
     with open(dump_name) as lines:
         box_bound = np.genfromtxt(islice(lines, 5, 8))
@@ -65,7 +68,6 @@ def define_bounds(box_bound):
         xz = box_bound[1, 2]
         yz = box_bound[2, 2]
         tilt = np.array([xy, xz, yz])
-
     xlo = box_bound[0, 0] - np.min(np.array([0, xy, xz, xy + xz]))
     xhi = box_bound[0, 1] - np.max(np.array([0, xy, xz, xy + xz]))
     ylo = box_bound[1, 0] - np.min(np.array([0, yz]))
@@ -73,7 +75,7 @@ def define_bounds(box_bound):
     zlo = box_bound[2, 0]
     zhi = box_bound[2, 1]
     untilted = np.array([[xlo, xhi], [ylo, yhi], [zlo, zhi]])
-    
+
     return untilted, tilt, box_type
 
 
@@ -101,3 +103,40 @@ def RemProb(data, CohEng, GbIndex):
     Excess_Eng_Tot = np.sum(Excess_Eng)
 
     return Excess_Eng/Excess_Eng_Tot
+
+
+def check_SC_reg(data, lat_par, rCut, non_p, tol_fix_reg, SC_tol):
+    """
+    Function to identify whether single crystal region on eaither side of the GB is
+    bigger than a tolerance (SC_tol)
+
+    Parameters
+    ------------
+    data :
+        Data object computed using OVITO I/O
+    lat_par :
+        Lattice parameter for the crystal being simulated
+    rCut :
+        Cut-off radius for computing Delaunay triangulations
+    non_pbc : int
+        The non-periodic direction. 0 , 1 or 2 which corresponds to
+        x, y and z direction, respectively.
+    tol_fix_reg : float
+        The user defined tolerance for the size of rigid translation region in lammps simulation.
+    SC_tol : float
+        The user defined tolerance for the minimum size of single crystal region.
+    Returns
+    ----------
+    SC_boolean :
+        A boolean list for low/top or left/right single crytal region. True means the width > SC_tol.
+    """
+    GbRegion, GbIndex, GbWidth, w_bottom_SC, w_top_SC = pdf.GB_finder(data, lat_par, non_p)
+
+    SC_boolean = [True, True]
+    w_bottom_SC = w_bottom_SC - tol_fix_reg
+    w_top_SC = w_top_SC - tol_fix_reg
+    if w_bottom_SC < SC_tol:
+        SC_boolean[0] = False
+    if w_top_SC < SC_tol:
+        SC_boolean[1] = False
+    return SC_boolean
